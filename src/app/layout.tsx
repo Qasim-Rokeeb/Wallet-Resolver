bb2f64f3e46e423da92e27c213c5d1f18cd171c0
Rokeeb Qasim <qasimrokeeb@gmail.com>
2025-08-09 21:35:04 +0000

    remove all the componenets that may be causing this syntax error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,12 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet" />
-      </head>
-      <body className="font-body antialiased">
+      <body>
         {children}
         <Toaster />
       </body>
